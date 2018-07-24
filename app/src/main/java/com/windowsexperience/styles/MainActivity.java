b516a281647f298c719c89df7e8b149215f6f8e2
Domi <domi04151309business@gmail.com>
2018-07-24 19:57:10 +0200

    Added ability to change the theme in the app

--- app/src/main/java/com/windowsexperience/styles/MainActivity.java
+++ app/src/main/java/com/windowsexperience/styles/MainActivity.java
@@ -1,13 +1,32 @@
 package com.windowsexperience.styles;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        Boolean darkMode = preferences.getBoolean("dark_mode", false);
+        if (darkMode)
+            setTheme(R.style.WindowsTheme_Dark);
+        else if (!darkMode)
+            setTheme(R.style.WindowsTheme_Light);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button pref = findViewById(R.id.pref);
+        pref.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Preferences.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/windowsexperience/styles/Preferences.java
@@ -0,0 +1,68 @@
+package com.windowsexperience.styles;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBar;
+import android.view.MenuItem;
+
+public class Preferences extends AppCompatPreferenceActivity {
+
+    SharedPreferences.OnSharedPreferenceChangeListener spChanged = new
+            SharedPreferences.OnSharedPreferenceChangeListener() {
+                @Override
+                public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                                      String key) {
+                    finish();
+                    startActivity(new Intent(Preferences.this, MainActivity.class));
+                }
+            };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        Boolean darkMode = preferences.getBoolean("dark_mode", false);
+        if (darkMode)
+            setTheme(R.style.WindowsTheme_Dark);
+        else if (!darkMode)
+            setTheme(R.style.WindowsTheme_Light);
+        super.onCreate(savedInstanceState);
+        setupActionBar();
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new GeneralPreferenceFragment())
+                .commit();
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(spChanged);
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class GeneralPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_general);
+            setHasOptionsMenu(true);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == android.R.id.home) {
+                startActivity(new Intent(getActivity(), Preferences.class));
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+    }
+}
